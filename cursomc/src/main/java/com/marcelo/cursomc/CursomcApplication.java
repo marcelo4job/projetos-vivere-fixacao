a5e112feab798b2bfd192c1f7249027e555cbeee
Marcelo Santos <marcelo4thejob@gmail.com>
2018-12-10 16:00:59 -0300

    arquivo modificado

--- cursomc/src/main/java/com/marcelo/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/marcelo/cursomc/CursomcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import sun.awt.geom.AreaOp;
 
 import java.util.Arrays;
