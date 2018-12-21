029c2aea13f91c2d9745ff026e900ccabe06843b
Marcelo Santos <marcelo.santos.costa@accenture.com>
2018-12-21 13:24:55 -0300

    Paginação com parametros opcionais na requisição

--- cursomc/src/main/java/com/marcelo/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/marcelo/cursomc/services/CategoriaService.java
@@ -3,8 +3,12 @@ package com.marcelo.cursomc.services;
 import com.marcelo.cursomc.domain.Categoria;
 import com.marcelo.cursomc.repositories.CategoriaRepository;
 import com.marcelo.cursomc.services.exception.ObjectNotFoundException;
+import com.sun.org.apache.xalan.internal.xsltc.dom.SimpleResultTreeImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -47,5 +51,11 @@ public class  CategoriaService {
     public List<Categoria> findAll(){
 
         return categoriaRepository.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return categoriaRepository.findAll(pageRequest);
     }
 }
